4f5506c81b8a8ea46257a6bb3cf1a41572019f16
Yann Vigara <yvigara@gmail.com>
2023-03-26 17:11:18 +0100

    Output generated GitHub public ssh-key

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "namespace" {
   description = "Flux Kubernetes namespace"
   value       = data.flux_sync.main.namespace
 }
+
+output "github_key" {
+  description = "The GitHub public ssh key"
+  value       = tls_private_key.main.public_key_openssh
+}
