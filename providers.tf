07e4618cd2e9ee4a77530b605401a9ee60819a55
Yann Vigara <yvigara@gmail.com>
2023-03-26 17:10:26 +0100

    Set Providers' minimum version

--- providers.tf
+++ providers.tf
@@ -1,21 +1,25 @@
 terraform {
   required_providers {
     flux = {
-      source = "fluxcd/flux"
+      source  = "fluxcd/flux"
+      version = ">= 0.25"
     }
     github = {
-      source = "integrations/github"
+      source  = "integrations/github"
+      version = ">= 5.0"
     }
     kubectl = {
-      source = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = ">= 5.0"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
+      version = ">= 4.0"
     }
   }
   required_version = ">= 1.0"
 }
-
